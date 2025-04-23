406d2b9ad571dcc94fec3dd912e9732cfa5be841
hfidelis <heitorc88@gmail.com>
2025-04-22 22:34:50 -0300

    initial commit 🌱

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Segment } from 'semantic-ui-react';
+import './App.css';
+import FormCliente from './views/cliente/FormCliente';
+
+function App() {
+  return (
+    <div className="App">
+
+      <FormCliente />
+
+      <div style={{marginTop: '6%'}}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
