17ce0aa58f7254b6d8989414f3eb1f8cb1125a1b
hfidelis <heitorc88@gmail.com>
2025-04-27 17:56:52 -0300

    feat(app): use routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import { Segment } from 'semantic-ui-react';
-import './App.css';
-import FormCliente from './views/cliente/FormCliente';
+import "./App.css";
+import Rotas from './Rotas';
+
+import { Segment } from "semantic-ui-react";
 
 function App() {
   return (
     <div className="App">
+      <Rotas />
 
-      <FormCliente />
-
-      <div style={{marginTop: '6%'}}>
-        <Segment vertical color='grey' size='tiny' textAlign='center'>
+      <div style={{ marginTop: "6%" }}>
+        <Segment vertical color="grey" size="tiny" textAlign="center">
           &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
         </Segment>
       </div>
-
     </div>
   );
 }
