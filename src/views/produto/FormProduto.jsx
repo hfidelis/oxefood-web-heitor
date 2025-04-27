95db6339090d47a6346cf5b71cc2e538fa2e03c6
hfidelis <heitorc88@gmail.com>
2025-04-27 17:58:02 -0300

    feat(views): Product view

---
+++ src/views/produto/FormProduto.jsx
@@ -0,0 +1,134 @@
+import axios from "axios";
+import React, { useState } from "react";
+import MenuSistema from "../../MenuSistema";
+
+import { Button, Container, Divider, Form, Icon } from "semantic-ui-react";
+
+export default function FormProduto() {
+  const [codigo, setCodigo] = useState("");
+  const [titulo, setTitulo] = useState("");
+  const [descricao, setDescricao] = useState("");
+  const [valorUnitario, setValorUnitario] = useState("");
+  const [tempoEntregaMinimo, setTempoEntregaMinimo] = useState("");
+  const [tempoEntregaMaximo, setTempoEntregaMaximo] = useState("");
+
+  function salvar() {
+    const produtoRequest = {
+      codigo: codigo,
+      titulo: titulo,
+      descricao: descricao,
+      valorUnitario: parseFloat(valorUnitario),
+      tempoEntregaMinimo: parseInt(tempoEntregaMinimo, 10),
+      tempoEntregaMaximo: parseInt(tempoEntregaMaximo, 10),
+    };
+
+    axios
+      .post("http://localhost:8080/api/produto", produtoRequest)
+      .then((response) => {
+        console.log("Produto cadastrado com sucesso.");
+      })
+      .catch((error) => {
+        console.log("Erro ao incluir o produto.");
+      });
+  }
+
+  return (
+    <div>
+      <MenuSistema tela={"produto"} />
+
+      <div style={{ marginTop: "3%" }}>
+        <Container textAlign="justified">
+          <h2>
+            <span style={{ color: "darkgray" }}>
+              Produto &nbsp;
+              <Icon name="angle double right" size="small" />
+            </span>
+            Cadastro
+          </h2>
+          <Divider />
+          <div style={{ marginTop: "4%" }}>
+            <Form>
+              <Form.Group widths="equal">
+                <Form.Input
+                  required
+                  fluid
+                  label="Código"
+                  maxLength="50"
+                  value={codigo}
+                  onChange={(e) => setCodigo(e.target.value)}
+                />
+                <Form.Input
+                  required
+                  fluid
+                  label="Título"
+                  maxLength="100"
+                  value={titulo}
+                  onChange={(e) => setTitulo(e.target.value)}
+                />
+              </Form.Group>
+              <Form.TextArea
+                label="Descrição"
+                placeholder="Descreva o produto..."
+                value={descricao}
+                onChange={(e) => setDescricao(e.target.value)}
+              />
+              <Form.Group widths="equal">
+                <Form.Input
+                  required
+                  fluid
+                  label="Valor Unitário"
+                  type="number"
+                  step="0.01"
+                  value={valorUnitario}
+                  onChange={(e) => setValorUnitario(e.target.value)}
+                />
+                <Form.Input
+                  required
+                  fluid
+                  label="Tempo Entrega Mínimo"
+                  type="number"
+                  value={tempoEntregaMinimo}
+                  onChange={(e) => setTempoEntregaMinimo(e.target.value)}
+                />
+                <Form.Input
+                  required
+                  fluid
+                  label="Tempo Entrega Máximo"
+                  type="number"
+                  value={tempoEntregaMaximo}
+                  onChange={(e) => setTempoEntregaMaximo(e.target.value)}
+                />
+              </Form.Group>
+            </Form>
+
+            <div style={{ marginTop: "4%" }}>
+              <Button
+                type="button"
+                inverted
+                circular
+                icon
+                labelPosition="left"
+                color="orange"
+              >
+                <Icon name="reply" />
+                Voltar
+              </Button>
+              <Button
+                inverted
+                circular
+                icon
+                labelPosition="left"
+                color="blue"
+                floated="right"
+                onClick={() => salvar()}
+              >
+                <Icon name="save" />
+                Salvar
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
+}
