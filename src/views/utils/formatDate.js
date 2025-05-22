ef89722cf2b7deb1fc76ab2ca4f59a2a3031c376
hfidelis <heitorc88@gmail.com>
2025-05-22 20:17:36 -0300

    fix(utils): folder location

--- src/views/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-export default function formatDate(dataParam) {
-  if (!dataParam) {
-    return "";
-  }
-
-  let arrayData = dataParam.split("-");
-
-  return arrayData[2] + "/" + arrayData[1] + "/" + arrayData[0];
-}
